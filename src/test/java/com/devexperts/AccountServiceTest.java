eade8e64af0c716750721a7c7b879952000758ad
Linh Hua <hualinh1012@gmail.com>
2023-10-06 22:01:39 +0700

    Add test class

---
+++ src/test/java/com/devexperts/AccountServiceTest.java
@@ -0,0 +1,14 @@
+package com.devexperts;
+
+import com.devexperts.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AccountServiceTest {
+
+    @Autowired
+    private AccountService accountService;
+
+
+}
